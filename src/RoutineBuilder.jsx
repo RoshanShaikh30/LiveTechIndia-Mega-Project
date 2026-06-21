03ddec86736cdfb4a75555b69ec40bbf9d6d2345
Roshan Shaikh <roshanrizwan1414@gmail.com>
2026-06-21 15:12:30 +0530

    Updated

--- src/RoutineBuilder.jsx
+++ src/RoutineBuilder.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function RoutineBuilder() {
   return (
     <div class="routine-builder">
-      <h1>Build Your Orbit</h1>
+      <h2>Build Your Orbit</h2>
       <p>
         Tell Orbit about your schedule, goals, and habits so it can create a routine that adapts to your life.
       </p>
@@ -41,7 +41,7 @@ function RoutineBuilder() {
           Homemaker
        </div>
 
-       <div className="choice-card">
+       <div className="choice-card other-card">
           Other
        </div>
 
